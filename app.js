e51221dcc7688e5d0366eb2d56113195f24f5325
Jacopo <jacopo.rodighiero@icloud.com>
2022-09-26 17:52:47 +0200

    more changes

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-gsap.fromTo(".line", { x: -40, fill: "blue" }, { x: 40, fill: "green" });
+gsap.fromTo(
+  ".navbar-container",
+  { x: 0, y: -100 },
+  { x: 0, y: 0, duration: 1 }
+);
+gsap.fromTo(
+  ".container-menu-right",
+  { x: -100, y: 0 },
+  { x: 0, y: 0, duration: 1 }
+);
+gsap.fromTo(".item1", { x: 0, y: 500 }, { x: 0, y: 0, duration: 2 });
+gsap.fromTo(".item2", { x: 550, y: 0 }, { x: 0, y: 0, duration: 1.5 });
+gsap.fromTo(
+  ".each-item",
+  { y: 10 },
+  { x: 0, y: -135, duration: 10, repeat: -1, yoyo: true, ease: "sine.inOut" }
+);
